56a62204b8b04f72e6966e1c17e0e1fa2c446b54
Edmilson Siqueira Braz Junior <100867319+Ksanim0@users.noreply.github.com>
2023-09-20 16:30:38 -0300

    Atualizar o primeiro projeto/update first project
    Projeto utilizando Hash em ruby para registrar veículos. Dados salvos temporariamente, ao encerrar o projeto, os dados são perdidos.

--- Ruby/FirstSecondProject/ruby.rb
+++ Ruby/FirstSecondProject/ruby.rb
@@ -1,17 +1,27 @@
 username = ENV['USER'] || ENV['LOGNAME']
 consecionaria = Hash.new
+cont = 0
 
 
 loop do
+puts "1- Registrar Veículo"
+puts "2- Visualizar veículos"
+puts "3- Editar veículos"
+puts "4- Apagar veículos"
+puts "5- Sair"
 puts "Bem vindo ao cadastro de veiculos senhor #{username}, selecione a opção que deseja para proseguir"
+
 opcSELEC = gets.chomp.to_i
 
 
 if opcSELEC == 1
+    puts "Digite o nome do carro a ser registrado: "
     carro = gets.chomp
+    puts "Digite a placa do carro que será registrado: "
     placa = gets.chomp
 
     consecionaria[carro] = placa
+    puts "Carros registrados:  #{consecionaria}"
 end
 
 if opcSELEC == 2
@@ -20,10 +30,31 @@ end
 
 if opcSELEC == 3
     puts consecionaria
-    puts "Indique qual carro você deseja excluir: "
-    delete = gets.chomp.to_i
-    consecionaria.delete(delete)
+    puts "Indique qual carro você deseja editar: "
+    mudarcarroNOME = gets.chomp
+
+    puts "Novo nome do carro: "
+    novoCarro = gets.chomp
+    puts "Nova placa: " 
+    placaNOVA = gets.chomp
+    consecionaria[novoCarro] = placaNOVA
+
+    consecionaria.delete(mudarcarroNOME)
+    
+    #puts "Nova placa: "
+    
 end
 
-opcSELEC == 0
+if opcSELEC == 4
+    puts consecionaria.keys
+    puts "Digite o nome do carro a ser excluído: "
+    deletar = gets.chomp
+    
+    valorDELLL=consecionaria.delete(deletar)
+end    
+
+if opcSELEC == 5
+    puts "Até Mais"
+    exit(0)
+end    
 end
